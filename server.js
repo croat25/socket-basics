23ca419ed74404293ef6ad7f59003d5454ceb608
Bruno Salapic <sala4460@mylaurier.ca>
2017-01-03 17:03:18 -0500

    added socket.io

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ var PORT = process.env.port || 3000; //initialze port and local host
 var express = require('express');// get express
 var app=express(); // initializr app to express variable
 var http=require('http').Server(app);//get the http to listen to the server.app
-
+var io=require('socket.io')(http);
 
 app.use(express.static(__dirname+'/public'));
+
+
+io.on('connection',function(){
+	console.log('User connected via socket.io!');
+});
 
 http.listen(PORT, function(){
 	console.log('serverstarted');
