828854d73de2270b7052be7b37b0440c9f4832dd
Bruno Salapic <sala4460@mylaurier.ca>
2017-01-03 16:42:23 -0500

    changed port varaible

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var PORT = process.env.port||3000; //initialze port and local host
+var PORT = process.env.port || 3000; //initialze port and local host
 var express = require('express');// get express
 var app=express(); // initializr app to express variable
 var http=require('http').Server(app);//get the http to listen to the server.app
