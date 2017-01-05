ec7584043d5492b5e2d3fab6480b09418b9760b7
Bruno Salapic <sala4460@mylaurier.ca>
2017-01-05 17:40:39 -0500

    add front end for to submit message

--- public/js/app.js
+++ public/js/app.js
@@ -7,3 +7,22 @@ socket.on('message',function(message){
 	console.log('New message:');
 	console.log(message.text);
 });
+
+
+//this is the index side form
+
+//# is used for ids
+// 'title ' is for straight up tags
+var $form = jQuery('#message-form');
+
+
+$form.on('submit',function(event){
+	event.preventDefault();//used to server
+
+	var $message=$form.find('input[name=message]');
+	socket.emit('message',{
+		text: $message.val()
+	});
+	//
+	$message.val("");
+});
