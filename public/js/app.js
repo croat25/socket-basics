7dc0ba1456ca06edba2266fd121a0c0780dad777
Bruno Salapic <sala4460@mylaurier.ca>
2017-01-05 19:07:31 -0500

    added names to messages

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,19 @@
 var socket = io();
+var name=getQueryVariable('name') || 'anon';
+var room=getQueryVariable('room');
 
+console.log(name+"wants to join"+room);
 socket.on('connect',function(){
 	console.log("connected to socket.io server!");
 });
 socket.on('message',function(message){
+	var $message=jQuery('.messages');
 	console.log('New message:');
 	var momenttimestamp=moment.utc(message.timestamp);
 	console.log(message.text);
 	// class u start with period
-	jQuery('.messages').append('<p><strong>'+momenttimestamp.local().format("h:mm a")+'</strong>'+message.text+'</p>')
+	$message.append('<p><strong>'+ message.name +''+ momenttimestamp.local().format("h:mm a")+'</strong></p>')
+	$message.append('<p>'+message.text+'</p>');
 });
 
 
@@ -24,6 +29,7 @@ $form.on('submit',function(event){
 
 	var $message=$form.find('input[name=message]');
 	socket.emit('message',{
+		name:name,
 		text: $message.val()
 	});
 	//
